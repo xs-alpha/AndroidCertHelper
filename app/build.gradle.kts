2965260d170d14e964ea81f3a20386b94b5412bc
xiaosheng <55642534+xs-alpha@users.noreply.github.com>
2024-02-13 21:03:33 +0800

    commit modify: 版本升级

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 2
-        versionName = "1.1"
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
